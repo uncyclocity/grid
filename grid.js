17811928dc38248ae0b15bddce378b9a5523b171
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2020-11-03 13:13:46 +0900

    jquery 적용

--- grid.js
+++ grid.js
@@ -6,57 +6,57 @@ var bright_greenblue = "#e9fffc";
 var white = "#ffffff";
 
 function cell_reset(){
-    var num = document.getElementById(tmp + "_1");
-    var name = document.getElementById(tmp + "_2");
-    var part = document.getElementById(tmp + "_3");
+    var num = $("#" + tmp + "_1");
+    var name = $("#" + tmp + "_2");
+    var part = $("#" + tmp + "_3");
     var grid_charColor = "#616161";
 
-    num.style.backgroundColor = white;
-    name.style.backgroundColor = white;
-    part.style.backgroundColor = white;
-    num.style.color = grid_charColor;
-    name.style.color = grid_charColor;
-    part.style.color = grid_charColor;
-    document.getElementById("output_area").value = "";
+    num.css("backgroundColor", white);
+    name.css("backgroundColor", white);
+    part.css("backgroundColor", white);
+    num.css("color", grid_charColor);
+    name.css("color", grid_charColor);
+    part.css("color", grid_charColor);
+    $("#output_area").val("");
 
     tmp = -1;
 }
 
 function disabled(){
-    var save_btn = document.getElementById("save_btn");
-    var name_box = document.getElementById("name");
-    var part_box = document.getElementById("part");
-    var hint1 = document.getElementById("hint1");
-    var hint2 = document.getElementById("hint2");
+    var save_btn = $("#save_btn");
+    var name_box = $("#name");
+    var part_box = $("#part");
+    var hint1 = $("#hint1");
+    var hint2 = $("#hint2");
     var disabled_color = "#b1b1b1"
 
-    save_btn.disabled = true;
-    name_box.disabled = true;
-    part_box.disabled = true;
+    save_btn.attr("disabled", true);
+    name_box.attr("disabled", true);
+    part_box.attr("disabled", true);
 
-    save_btn.style.borderColor = disabled_color;
-    save_btn.style.color = disabled_color;
-    hint1.style.color = disabled_color;
-    hint2.style.color = disabled_color;
+    save_btn.css("borderColor", disabled_color);
+    save_btn.css("color", disabled_color);
+    hint1.css("color", disabled_color);
+    hint2.css("color", disabled_color);
 
 }
 
 function abled(){
-    var save_btn = document.getElementById("save_btn");
-    var name_box = document.getElementById("name");
-    var part_box = document.getElementById("part");
-    var hint1 = document.getElementById("hint1");
-    var hint2 = document.getElementById("hint2");
+    var save_btn = $("#save_btn");
+    var name_box = $("#name");
+    var part_box = $("#part");
+    var hint1 = $("#hint1");
+    var hint2 = $("#hint2");
     var abled_color = greenblue;
 
-    save_btn.disabled = false;
-    name_box.disabled = false;
-    part_box.disabled = false;
+    save_btn.attr("disabled", false);
+    name_box.attr("disabled", false);
+    part_box.attr("disabled", false);
 
-    save_btn.style.borderColor = abled_color;
-    save_btn.style.color = abled_color;
-    hint1.style.color = abled_color;
-    hint2.style.color = abled_color;
+    save_btn.css("borderColor" , abled_color);
+    save_btn.css("color", abled_color);
+    hint1.css("color", abled_color);
+    hint2.css("color", abled_color);
 }
 
 function isNotEmpty(name, part){
@@ -73,18 +73,18 @@ function isNotEmpty(name, part){
 }
 
 function clear(){
-    document.getElementById("name").value = "";
-    document.getElementById("part").value = "";
+    $("#name").val("");
+    $("#part").val("");
 }
 
 function add(){
-    var add_btn = document.getElementById("add_btn");
+    var add_btn = $("#add_btn");
 
     if(sw!=1){
         sw = 1;      
 
-        add_btn.style.backgroundColor = greenblue;
-        add_btn.style.color = bright_greenblue;
+        add_btn.css("backgroundColor", greenblue);
+        add_btn.css("color", bright_greenblue);
 
         if(tmp != -1)
             cell_reset();
@@ -96,8 +96,8 @@ function add(){
     else{
         sw = 0;
 
-        add_btn.style.backgroundColor = bright_greenblue;
-        add_btn.style.color = greenblue;
+        add_btn.css("backgroundColor", bright_greenblue);
+        add_btn.css("color", greenblue);
 
         disabled();
 
@@ -107,58 +107,58 @@ function add(){
 
 function cell_focus(grid_rowcnt){
     if(sw!=2){
-        var add_btn = document.getElementById("add_btn");
+        var add_btn = $("#add_btn");
 
         if(sw == 1){
-            add_btn.style.backgroundColor = bright_greenblue;
-            add_btn.style.color = greenblue;
+            add_btn.css("backgroundColor", bright_greenblue);
+            add_btn.css("color", greenblue);
         }
 
         sw = 2;
 
         abled();
 
-        var num = document.getElementById(grid_rowcnt + "_1");
-        var name = document.getElementById(grid_rowcnt + "_2");
-        var part = document.getElementById(grid_rowcnt + "_3");
-        var output = document.getElementById("output_area");
+        var num = $("#" + grid_rowcnt + "_1");
+        var name = $("#" + grid_rowcnt + "_2");
+        var part = $("#" + grid_rowcnt + "_3");
+        var output = $("#output_area");
 
-        num.style.backgroundColor = greenblue;
-        name.style.backgroundColor = greenblue;
-        part.style.backgroundColor = greenblue;
-        num.style.color = white;
-        name.style.color = white;
-        part.style.color = white;
+        num.css("backgroundColor", greenblue);
+        name.css("backgroundColor", greenblue);
+        part.css("backgroundColor", greenblue);
+        num.css("color", white);
+        name.css("color", white);
+        part.css("color", white);
 
         tmp = grid_rowcnt;
 
-        document.getElementById("name").value = db[grid_rowcnt-1][1];
-        document.getElementById("part").value = db[grid_rowcnt-1][2];
+        $("#name").val(db[grid_rowcnt-1][1]);
+        $("#part").val(db[grid_rowcnt-1][2]);
 
-        output.value = db[grid_rowcnt-1][1] + ", " + db[grid_rowcnt-1][2];
+        output.val(db[grid_rowcnt-1][1] + ", " + db[grid_rowcnt-1][2]);
 
     }else if(grid_rowcnt != tmp){
 
         cell_reset();
 
-        var num = document.getElementById(grid_rowcnt + "_1");
-        var name = document.getElementById(grid_rowcnt + "_2");
-        var part = document.getElementById(grid_rowcnt + "_3");
-        var output = document.getElementById("output_area");
+        var num = $("#" + grid_rowcnt + "_1");
+        var name = $("#" + grid_rowcnt + "_2");
+        var part = $("#" + grid_rowcnt + "_3");
+        var output = $("#output_area");
 
-        num.style.backgroundColor = greenblue;
-        name.style.backgroundColor = greenblue;
-        part.style.backgroundColor = greenblue;
-        num.style.color = white;
-        name.style.color = white;
-        part.style.color = white;
+        num.css("backgroundColor", greenblue);
+        name.css("backgroundColor", greenblue);
+        part.css("backgroundColor", greenblue);
+        num.css("color", white);
+        name.css("color", white);
+        part.css("color", white);
 
         tmp = grid_rowcnt;
 
-        document.getElementById("name").value = db[grid_rowcnt-1][1];
-        document.getElementById("part").value = db[grid_rowcnt-1][2];
+        $("#name").val(db[grid_rowcnt-1][1]);
+        $("#part").val(db[grid_rowcnt-1][2]);
 
-        output.value = db[grid_rowcnt-1][1] + ", " + db[grid_rowcnt-1][2];
+        output.val(db[grid_rowcnt-1][1] + ", " + db[grid_rowcnt-1][2]);
     }
     else{
         
@@ -174,23 +174,23 @@ function cell_focus(grid_rowcnt){
 }
 
 function save(){
-    var name = document.getElementById("name").value;
-    var part = document.getElementById("part").value;
+    var name = $("#name").val();
+    var part = $("#part").val();
 
     switch(sw){
         case 1:
             if(isNotEmpty(name, part)){
-                var grid = document.getElementById("grid_main");
-                var grid_rowcnt = grid.rows.length;
-                var grid_newrow = grid.insertRow(grid_rowcnt);
-                var newNum = grid_newrow.insertCell(0);
-                var newName = grid_newrow.insertCell(1);
-                var newPart = grid_newrow.insertCell(2);
-    
-                newNum.innerHTML = "<center><div class='cell' id='"+ grid_rowcnt +"_1' onclick='cell_focus("+ grid_rowcnt +")'>" + grid_rowcnt + "</div></center>";
-                newName.innerHTML = "<center><div class='cell' id='"+ grid_rowcnt +"_2' onclick='cell_focus("+ grid_rowcnt +")'>" + name + "</div></center>";
-                newPart.innerHTML = "<center><div class='cell' id='"+ grid_rowcnt +"_3' onclick='cell_focus("+ grid_rowcnt +")'>" + part + "</div></center>";
+                var grid = $("#grid_main");
+                var grid_rowcnt = $("#grid_main tr").length;
+                var html = "";
+                html += "<tr>";
+                html += "<td><center><div class='cell' id='"+ grid_rowcnt +"_1' onclick='cell_focus("+ grid_rowcnt +")'>" + grid_rowcnt + "</div></center></td>";
+                html += "<td><center><div class='cell' id='"+ grid_rowcnt +"_2' onclick='cell_focus("+ grid_rowcnt +")'>" + name + "</div></center></td>";
+                html += "<td><center><div class='cell' id='"+ grid_rowcnt +"_3' onclick='cell_focus("+ grid_rowcnt +")'>" + part + "</div></center></td>";
+                html += "</tr>"
+                grid.append(html);
                 db[db.length] = [grid_rowcnt, name, part];
+
                 alert('추가완료');
 
                 clear();
@@ -198,9 +198,11 @@ function save(){
             break;
         case 2:
             if(isNotEmpty(name, part)){
-                document.getElementById(tmp + "_2").innerText = name;
-                document.getElementById(tmp + "_3").innerText = part;
+                $("#" + tmp + "_2").html(name);
+                $("#" + tmp + "_3").html(part);
                 db[tmp-1] = [tmp, name, part];
+                $("#output_area").val(name + ", " + part);
+
                 alert('수정완료');
             }
             break;
